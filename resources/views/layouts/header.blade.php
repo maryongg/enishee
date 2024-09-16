6e67e27671c5ee98ba987d92072e585b9985d6e9
maryongg <167822923+maryongg@users.noreply.github.com>
2024-09-16 15:42:40 +0900

    トップページ

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>TOP</title>
+</head>
+<body>
+    <div class="bg-white py-6 sm:py-8 lg:py-12">
+        <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
